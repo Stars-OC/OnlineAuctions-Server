c73a41aef8b6897fdc388a7aba9857c9afa8755e
Clusters_stars <Clusters_stars@qq.com>
2024-01-28 14:04:41 +0800

    first commit

---
+++ src/main/java/com/onlineauctions/onlineauctions/OnlineAuctionsApplication.java
@@ -0,0 +1,13 @@
+package com.onlineauctions.onlineauctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineAuctionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineAuctionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onlineauctions/onlineauctions/OnlineAuctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineauctions.onlineauctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAuctionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
